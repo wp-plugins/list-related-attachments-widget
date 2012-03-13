1a0dd9cbd634b3a3b84f991e462454a4994f351a
baden03 <baden03@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-13 15:09:46 +0000

    tagging version 1.6.1
    git-svn-id: https://plugins.svn.wordpress.org/list-related-attachments-widget/trunk@518513 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.6.1/1.1/list-rel-attach.php
@@ -0,0 +1,104 @@
+<?php
+/*
+	Plugin Name: List Related Attachments Widget
+	Plugin URI: http://twinpictures.de
+	Description: Display a list of related attachments linked to the current post or page
+	Version: 1.1
+	Author: Twinpictures
+	Author URI: http://www.twinpictures.de/related-attachments/
+*/
+
+function widget_listattach_init() {
+
+	if ( !function_exists('register_sidebar_widget') )
+		return;
+		
+	function sanitize_attachment($name) {
+    	$name = strtolower($name); // all lowercase
+    	$name = preg_replace('/[^a-z0-9 ]/','', $name); // nothing but a-z 0-9 and spaces
+    	$name = preg_replace('/\s+/','-', $name); // spaces become hyphens
+    	return $name;
+  	}
+
+	// Options and default values for this widget
+	function widget_listattach_options() {
+		return array(
+			'title' => "Attachments",
+			'count' => -1,
+			'type' => "application"
+		);
+	}
+
+	function widget_listattach($args) {
+		global $wpdb, $post;
+
+		extract($args);
+		$options = array_merge(widget_listattach_options(), get_option('widget_listattach'));
+		unset($options[0]); //returned by get_option(), but we don't need it
+		$args = array(
+			'post_type' => 'attachment',
+			'post_mime_type' => $options['type'],
+			'numberposts' => $options['count'],
+			'post_parent' => $post->ID
+		); 
+		$attachments = get_posts($args);
+		if ($attachments) {
+			echo $before_widget . $before_title .$options['title'] . $after_title;
+			echo '<ul>';
+			foreach ($attachments as $attachment) {
+				//echo '<li>'.$attachment->post_title.'</li>';
+				//the_attachment_link($attachment->ID, false);
+				echo '<li>'.wp_get_attachment_link($attachment->ID).'</li>';
+				
+			}
+			echo '</ul>'.$after_widget;
+		}
+		
+	}
+
+	// This is the function that outputs the form to let the users edit
+	// the widget's title. It's an optional feature that users cry for.
+	function widget_listattach_control() {
+		// Each widget can store and retrieve its own options.
+		// Here we retrieve any options that may have been set by the user
+		// relying on widget defaults to fill the gaps.
+		if(($options = get_option('widget_listattach')) === FALSE) $options = array();
+
+		$options = array_merge(widget_listattach_options(), $options);
+		unset($options[0]); //returned by get_option(), but we don't need it
+
+		// If user is submitting custom option values for this widget
+		if ( $_POST['listattach-submit'] ) {
+			// Remember to sanitize and format use input appropriately.
+			foreach($options as $key => $value)
+				if(array_key_exists('listattach-'.sanitize_attachment($key), $_POST))
+				$options[$key] = strip_tags(stripslashes($_POST['listattach-'.sanitize_attachment($key)]));
+
+			// Save changes
+			update_option('widget_listattach', $options);
+		}
+		
+		// title option
+		echo '<p style="text-align:left"><label for="listattach-title">Title: <input style="width: 200px;" id="listattach-title" name="listattach-title" type="text" value="'.$options['title'].'" /></label></p>';
+		// count option
+		echo '<p style="text-align:left"><label for="listattach-count">Count: <input style="width: 70px;" id="listattach-count" name="listattach-count" type="text" value="'.$options['count'].'" /></label> (-1 = all)</p>';
+
+		// type option
+		echo '<p style="text-align:left"><label for="listattach-type">Attachment Mime/Type: <input style="width: 200px;" id="listattach-type" name="listattach-type" type="text" value="'.$options['type'].'" /></label><br/>';
+		echo '<a href="http://en.wikipedia.org/wiki/MIME_type#List_of_common_media_types" target="_blank">list of common mime/types</a></p>';
+		// Submit
+		echo '<input type="hidden" id="listattach-submit" name="listattach-submit" value="1" />';
+	}
+	
+	// This registers our widget so it appears with the other available
+	// widgets and can be dragged and dropped into any active sidebars.
+	register_sidebar_widget('Related Attachments', 'widget_listattach');
+
+	// This registers our optional widget control form.
+	register_widget_control('Related Attachments', 'widget_listattach_control');
+}
+
+// Run code later in case this loads prior to any required plugins.
+add_action('plugins_loaded', 'widget_listattach_init');
+
+?>
---
+++ 1.6.1/1.3/readme.txt
@@ -0,0 +1,72 @@
+=== Plugin Name ===
+Contributors: Twinpictures
+Donate link: http://www.twinpictures.de/related-attachments/
+Tags: widget, list, related, attachments, sidebar, uploaded, files, shortcode
+Requires at least: 2.0.2
+Tested up to: 3.0.1
+Stable tag: 1.3
+
+List Related Attachments is a sidebar widget that will display a list of filtered attachments related to the current post.
+
+== Description ==
+
+List Related Attachments is a sidebar widget (and as of 1.2 shortcode function) that will display a filtered list of all related attachments linked to current post or page. The widget options are: title, number of attachments to display and type of attachment to display by mime/type. The shortcode is options are "title" and "count" and is placed inside of a post or page like so: `[list-related-attach type="application/zip" count="3" orderby="menu_order ID" order="ASC"]`.
+
+== Installation ==
+
+1. Old-school: upload the `List Related Attachments` folder to the `/wp-content/plugins/` directory via FTP.  Hipster: Add the plugin via the WordPress Plugins menu.
+1. Activate the Plugin
+1. Add the Widget to the desired sidebar in the WordPress Widgets menu.
+1. Configure the `Listed Related Attachments` widget options.
+1. Add a the shortcode to your post like so: `[list-related-attach type="application/zip" count="3"]`
+1. Test that the this plugin meets your demanding needs.
+1. Rate the plugin and verify that it works at wordpress.org.
+1. Leave a comment regarding bugs, feature request, cocktail recipes at www.twinpictures.de/related-attachments/
+
+== Frequently Asked Questions ==
+
+= What is the meaning of life, the universe and everything? =
+
+42
+
+= What are the default shortcode attributes? =
+
+1. type = "application" (so basically everything)
+1. count = "-1" (so everything)
+1. orderby = "date" (by the date the attachment was added)
+1. order = "DESC" (Descending in the newest to oldest direction)
+
+= I am going to a respectable cocktail bar, can you recommend a drink I should try? =
+
+Gin Basel Smash
+
+== Screenshots ==
+
+1. Screen shot screenshot-1.png shows the available options of the `List Related Attachments` including widget title, count and type of attachments to display.
+
+== Changelog ==
+
+= 1.3 =
+* Expanded the options to include orderby and order... for *Ultimater Flexibility* -- yes, Ultimater is not a real word, but just go with it, OK?
+
+= 1.2 =
+* Added shortcode so that related attachments can be listed withing the page loop.  Listing related attachments is not just for sidebar widgets anymore.
+
+= 1.1 =
+* Changed mime/type to a text input field for *Ultimate Flexibility*.
+* Pimped this readme.txt file.
+* Added the plugin to wordpress.org.
+
+= 1.0 =
+* The plugin came to be.
+
+== Upgrade Notice ==
+
+= 1.3 =
+Expands the plugin to include orderby and order options.
+
+= 1.2 =
+Introduces the [list-related-attach] shortcode for listing related attachments within posts & pages
+
+= 1.1 =
+Upgrade today to take advantage of Ultimate Flexibility by manually entering the mime/type of the attachments to list.
