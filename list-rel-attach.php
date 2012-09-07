f5eb5e21486e1d63c103f31b073095becc61e9da
baden03 <baden03@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-07 14:44:47 +0000

    tagging versin 1.8
    git-svn-id: https://plugins.svn.wordpress.org/list-related-attachments-widget/tags/1.8@595966 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- list-rel-attach.php
+++ list-rel-attach.php
@@ -1,10 +1,10 @@
 <?php
 /*
 	Plugin Name: List Related Attachments
-	Plugin URI: hhttp://plugins.twinpictures.de/plugins/list-related-attachments/
+	Plugin URI: http://plugins.twinpictures.de/plugins/list-related-attachments/
 	Description: Display a filtered list of all related attachments linked to the current post or page
-	Version: 1.6.1
-	Author: twinpictures
+	Version: 1.8
+	Author: Twinpictures
 	Author URI: http://www.twinpictures.de/
 	License: GPL2
 */
@@ -25,149 +25,104 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-//the widget
-function widget_listattach_init() {
-
-	if ( !function_exists('register_sidebar_widget') )
-		return;
-		
-	function sanitize_attachment($name) {
-    	$name = strtolower($name); // all lowercase
-    	$name = preg_replace('/[^a-z0-9 ]/','', $name); // nothing but a-z 0-9 and spaces
-    	$name = preg_replace('/\s+/','-', $name); // spaces become hyphens
-    	return $name;
-  	}
-
-	// Options and default values for this widget
-	function widget_listattach_options() {
-		return array(
-			'title' => 'Attachments',
-			'count' => -1,
-			'type' => 'application',
-			'orderby' => 'date',
-			'order' => 'DESC',
-			'display' => 'title'
-		);
-	}
-
-	function widget_listattach($args) {
+class listAttach extends WP_Widget {
+    /** constructor */
+    function listAttach() {
+		$widget_ops = array('classname' => 'listAttach', 'description' => __('Display a filtered list of related attachments for the current post') );
+		$this->WP_Widget('listAttach', 'List Related Attachments', $widget_ops);
+    }
+	
+    /** Widget */
+    function widget($args, $instance) {
 		global $wpdb, $post;
-
 		extract($args);
-		$options = array_merge(widget_listattach_options(), get_option('widget_listattach'));
-		unset($options[0]); //returned by get_option(), but we don't need it
-
+		$title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);
+		$count = empty($instance['count']) ? '-1' : apply_filters('widget_count', $instance['count']);
+		$type = empty($instance['type']) ? 'application' : apply_filters('widget_type', $instance['type']);
+		$orderby = empty($instance['orderby']) ? 'date' : apply_filters('widget_orderby', $instance['orderby']);
+		$order = empty($instance['order']) ? 'DESC' : apply_filters('widget_order', $instance['order']);
+		$display = empty($instance['display']) ? 'title' : apply_filters('widget_display', $instance['display']);
+		$target = empty($instance['target']) ? 'self' : apply_filters('widget_target', $instance['target']);
+		
 		$args = array(
 			'post_type' => 'attachment',
-			'post_mime_type' => $options['type'],
-			'numberposts' => $options['count'],
-			'orderby' => $options['orderby'],
-			'order' => $options['order'],
+			'post_mime_type' => $type,
+			'numberposts' => $count,
+			'orderby' => $orderby,
+			'order' => $order,
 			'post_parent' => $post->ID
 		);
 		
 		$attachments = get_children($args);
 		if ($attachments) {
-			
-			echo $before_widget . $before_title . $options['title'] . $after_title;
+			echo $before_widget;
+			if($title){
+				echo $before_title . $title . $after_title;
+			}
 			echo '<ul>';
+			$link_target = '';
+			if($target != 'self'){
+				$link_target = 'target="_blank"';
+			}
 			foreach ($attachments as $attachment) {
 				$mime_parts = explode("/", $attachment-> post_mime_type);
 				$mime_class = 'class="mime-'.$mime_parts[count($mime_parts)-1].'"';
-				$descr = 'post_title'; 
-				if($options['display'] == 'caption'){
-					$descr = 'post_excerpt';
-				}
-				else if($options['display'] == 'description'){
-					$descr = 'post_content';  
-				}
-				if($attachment->$descr){
-					echo '<li '.$mime_class.'><a href="'.wp_get_attachment_url($attachment->ID).'">'.$attachment->$descr.'</a></li>';     
-				}
-				else{
-					echo '<li '.$mime_class.'>'.wp_get_attachment_link($attachment->ID).'</li>';
-				}
+				//replace title
+				$display_str = str_replace('title', $attachment->post_title, $display);
+				//replace caption
+				$display_str = str_replace('caption', $attachment->post_excerpt, $display_str);
+				//replace description
+				$display_str = str_replace('description', $attachment->post_content, $display_str);
+				echo '<li '.$mime_class.'><a href="'.wp_get_attachment_url($attachment->ID).'" '.$link_target.'>'.$display_str.'</a></li>';     
 			}
 			echo '</ul>'.$after_widget;
 		}
-		
-	}
-
-	// This is the function that outputs the form to let the users edit
-	// the widget's title. It's an optional feature that users cry for.
-	function widget_listattach_control() {
-		// Each widget can store and retrieve its own options.
-		// Here we retrieve any options that may have been set by the user
-		// relying on widget defaults to fill the gaps.
-		if(($options = get_option('widget_listattach')) === FALSE) $options = array();
-
-		$options = array_merge(widget_listattach_options(), $options);
-		unset($options[0]); //returned by get_option(), but we don't need it
-
-		// If user is submitting custom option values for this widget
-		if ( $_POST['listattach-submit'] ) {
-			// Remember to sanitize and format use input appropriately.
-			foreach($options as $key => $value)
-				if(array_key_exists('listattach-'.sanitize_attachment($key), $_POST))
-				$options[$key] = strip_tags(stripslashes($_POST['listattach-'.sanitize_attachment($key)]));
-
-			// Save changes
-			update_option('widget_listattach', $options);
-		}
-		
-		// title option
-		echo '<p style="text-align:left"><label for="listattach-title">Title: <input style="width: 200px;" id="listattach-title" name="listattach-title" type="text" value="'.$options['title'].'" /></label></p>';
-		// count option
-		echo '<p style="text-align:left"><label for="listattach-count">Count: <input style="width: 70px;" id="listattach-count" name="listattach-count" type="text" value="'.$options['count'].'" /></label> (-1 = all)</p>';
-
-		// type option
-		echo '<p style="text-align:left"><label for="listattach-type">Attachment Mime/Type: <input style="width: 200px;" id="listattach-type" name="listattach-type" type="text" value="'.$options['type'].'" /></label><br/>';
-		echo '<a href="http://en.wikipedia.org/wiki/MIME_type#List_of_common_media_types" target="_blank">list of common mime/types</a></p>';
-		
-        // order option
-		echo '<p style="text-align:left"><label for="listattach-ordeby">Order By: <input style="width: 200px;" id="listattach-orderby" name="listattach-orderby" type="text" value="'.$options['orderby'].'" /></label><br/>';
-        echo '<a href="http://codex.wordpress.org/Function_Reference/query_posts#Orderby_Parameters" target="_blank">list of orderby values</a></p>';
-		
-        // order direction option
-		echo '<p style="text-align:left"><label for="listattach-order">Order Direction: <input style="width: 200px;" id="listattach-order" name="listattach-order" type="text" value="'.$options['order'].'" /></label><br/>';
-		echo 'Valid values: ASC or DESC</p>';
-                                
-        // display option
-		echo '<p style="text-align:left">Display: ';
-		echo '<select name="listattach-display" id="listattach-display">';
-		$option_arr = array('title', 'caption', 'description');
-		foreach($option_arr AS $opt){
-			$selected = '';
-			if($options['display'] == $opt){
-				$selected = 'SELECTED';
-			}
-			echo '<option value="'.$opt.'" '.$selected.'>'.$opt.'</option>';
-		}
-		echo '</select></p>';
-		
-		// scope
-		/*
-		$checked = '';
-		if($options['scope'] == 'post'){
-			$checked = 'CHECKED';
-		}
-		echo '<p style="text-align:left"><input type="checkbox" id="listattach-scope" name="listattach-scope" value="'.$options['scope'].'" '.$checked.'/> <label for="listattach-scope">Limit Scope To Current Post</label></p>';
-		*/
-		
-		// Submit
-		echo '<input type="hidden" id="listattach-submit" name="listattach-submit" value="1" />';
 	}
 	
-	// This registers our widget so it appears with the other available
-	// widgets and can be dragged and dropped into any active sidebars.
-	register_sidebar_widget('Related Attachments', 'widget_listattach');
+	/** Update */
+    function update($new_instance, $old_instance) {
+		$instance = array_merge($old_instance, $new_instance);
+		return array_map('mysql_real_escape_string', $instance);
+    }
+	
+	function form($instance) {
+		$title = empty($instance['title']) ? '' : apply_filters('widget_title', $instance['title']);
+		$count = empty($instance['count']) ? '-1' : apply_filters('widget_count', $instance['count']);
+		$type = empty($instance['type']) ? 'application' : apply_filters('widget_type', $instance['type']);
+		$orderby = empty($instance['orderby']) ? 'date' : apply_filters('widget_orderby', $instance['orderby']);
+		$order = empty($instance['order']) ? 'DESC' : apply_filters('widget_order', $instance['order']);
+		$display = empty($instance['display']) ? 'title' : apply_filters('widget_display', $instance['display']);
+		$target = empty($instance['target']) ? 'self' : apply_filters('widget_target', $instance['target']);
+		?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('count'); ?>"><?php _e('Count:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>" type="text" value="<?php echo $count; ?>" /></label>
+		<span class="description"><?php _e('(-1 = all)'); ?></span></p>
+		<p><label for="<?php echo $this->get_field_id('type'); ?>"><?php _e('Attachment Mime/Type:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('type'); ?>" name="<?php echo $this->get_field_name('type'); ?>" type="text" value="<?php echo $type; ?>" /></label>
+		<a class="description" href="http://en.wikipedia.org/wiki/MIME_type#List_of_common_media_types" target="_blank"><?php _e('list of common mime/types'); ?></a></p>
+		<p><label for="<?php echo $this->get_field_id('orderby'); ?>"><?php _e('Order By:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('orderby'); ?>" name="<?php echo $this->get_field_name('orderby'); ?>" type="text" value="<?php echo $orderby; ?>" /></label>
+		<a class="description" href="http://codex.wordpress.org/Function_Reference/query_posts#Orderby_Parameters" target="_blank"><?php _e('list of orderby values'); ?></a></p>
+		<p><label for="<?php echo $this->get_field_id('order'); ?>"><?php _e('Order Direction:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('order'); ?>" name="<?php echo $this->get_field_name('order'); ?>" type="text" value="<?php echo $order; ?>" /></label>
+		<span class="description"><?php _e('Valid values: ASC or DESC'); ?></span></p>
+		<p><label for="<?php echo $this->get_field_id('display'); ?>"><?php _e('Display:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('display'); ?>" name="<?php echo $this->get_field_name('display'); ?>" type="text" value="<?php echo $display; ?>" /></label>
+		<span class="description"><?php _e('title, caption and/or descripton'); ?></span></p>
+        <p><?php _e('Target:'); ?> <select name="<?php echo $this->get_field_name('target'); ?>" id="<?php echo $this->get_field_name('target'); ?>">
+		<?php	
+		    $option_arr = array('self', 'blank');
+			foreach($option_arr AS $opt){
+				$selected = '';
+				if($target == $opt){
+					$selected = 'SELECTED';
+				}
+				echo '<option value="'.$opt.'" '.$selected.'>'.$opt.'</option>';
+			}
+		?>
+	    </select></p>
+		<?php
+	}
+} // class listAttach
 
-	// This registers our optional widget control form.
-	register_widget_control('Related Attachments', 'widget_listattach_control');
-}
-
-// Run code later in case this loads prior to any required plugins.
-add_action('plugins_loaded', 'widget_listattach_init');
+// register listAttach widget
+add_action('widgets_init', create_function('', 'return register_widget("listAttach");'));
 
 //the short code
 function listattach($atts) {
@@ -178,8 +133,8 @@ function listattach($atts) {
 		'count' => -1,
 		'orderby' => 'date',
 		'order' => 'DESC',
-		'show' => 'post_title',
-		'scope' => 'post'
+		'show' => 'title',
+		'target' => 'self'
 	), $atts));
 	
 	$args = array(
@@ -194,30 +149,28 @@ function listattach($atts) {
 	$attachments = get_children($args);
 	if ($attachments) {
 		$lra = '<ul class = "list-related-attach '.$show.'">';
+		$link_target = '';
+		if($target != 'self'){
+			$link_target = 'target="_blank"';
+		}
 		foreach ($attachments as $attachment) {
 			//let's add swill's sexy mime type class, shall we?
 			$mime_parts = explode("/", $attachment->post_mime_type);
             $mime_class = 'class="mime-'.$mime_parts[count($mime_parts)-1].'"';
 			$descr = 'post_title';
-			if($show == 'caption'){
-				$descr = 'post_excerpt';
-			}
-			else if($show == 'description'){
-				$descr = 'post_content';  
-			}
-			if($attachment->$descr){
-				$lra .= '<li '.$mime_class.'><a href="'.wp_get_attachment_url($attachment->ID).'">'.$attachment->$descr.'</a></li>';     
-			}
-			else{
-				$lra .= '<li '.$mime_class.'>'.wp_get_attachment_link($attachment->ID).'</li>';
-			}
+			//replace title
+			$display_str = str_replace('title', $attachment->post_title, $show);
+			//replace caption
+			$display_str = str_replace('caption', $attachment->post_excerpt, $display_str);
+			//replace description
+			$display_str = str_replace('description', $attachment->post_content, $display_str);
+			$lra .= '<li '.$mime_class.'><a href="'.wp_get_attachment_url($attachment->ID).'" '.$link_target.'>'.$display_str.'</a></li>'; 
 		}
 		wp_reset_postdata();
 		$lra .= '</ul>';
 	}
 	return $lra;
 }
-
 add_shortcode('list-related-attach', 'listattach');
 
 ?>
